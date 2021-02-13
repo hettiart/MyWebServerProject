ccf787c6aec2cdf117bb67f342636c264b8ec584
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-14 02:20:05 +0530

    adding access keys of S3 in raw format

--- script1.sh
+++ script1.sh
@@ -12,6 +12,6 @@
 ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
 /usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
 curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+AWS_ACCESS_KEY_ID=AKIAZUQNAOFSFWRKGIIZ
+AWS_SECRET_ACCESS_KEY=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
 aws s3 cp results1.txt s3://nginx-web-server-bucket/
