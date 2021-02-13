ba670a0e046d63a5110f1fdd75c78b425885ed5a
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-13 20:34:53 +0530

    script upload with s3 change

--- script1.sh
+++ script1.sh
@@ -5,6 +5,6 @@
 ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
 /usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
 curl -I "http://54.226.64.162/" 2>&1 >> results.txt
-#aws s3 cp results.txt s3://nginx-web-server-bucket/
+aws s3 cp results.txt s3://nginx-web-server-bucket/
 #sleep 10
 #done
