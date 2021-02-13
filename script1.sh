605be76f1e4ecc6849ca882ac4ced94110a77481
hettiart <hettiart@LAPTOP-78IE47PR.localdomain>
2021-02-14 00:14:36 +0530

    changes for results1 file naming

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@
 #every 10 seconds ssh to public server/check web server alive if not start
 ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
 /usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
-curl -I "http://54.226.64.162/" 2>&1 >> a.txt
-aws s3 cp a.txt s3://nginx-web-server-bucket/
+curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
+aws s3 cp results1.txt s3://nginx-web-server-bucket/
 #sleep 10
 #done
