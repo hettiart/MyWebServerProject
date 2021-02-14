190e4254f24a566317df9b9d4734f2ed75f798a1
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-15 01:45:16 +0530

    finalizing script1

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
-/usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
+/usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" || sudo systemctl start nginx 
 curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
 echo "Starting backup process: "
 aws s3 cp results1.txt s3://nginx-web-server-bucket/
