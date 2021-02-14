cc37c521f9c9c199eae600f76b31540822bdab8c
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-14 14:06:00 +0530

    remove access keys in public git hub

--- script1.sh
+++ script1.sh
@@ -9,9 +9,3 @@
 #sleep 10
 #done
 
-ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
-/usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
-curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
-AWS_ACCESS_KEY_ID=AKIAZUQNAOFSFWRKGIIZ
-AWS_SECRET_ACCESS_KEY=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
-aws s3 cp results1.txt s3://nginx-web-server-bucket/
