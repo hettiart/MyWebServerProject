41a000459d8329027963de61dd7e2e168d87d192
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-14 16:48:20 +0530

    changing the job with new access keys

--- script1.sh
+++ script1.sh
@@ -2,10 +2,13 @@
 #while true
 #do
 #every 10 seconds ssh to public server/check web server alive if not start
-#ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
-#/usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
-#curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
-#aws s3 cp results1.txt s3://nginx-web-server-bucket/
+ssh -i kplabs-terraform.pem ec2-user@54.226.64.162
+/usr/bin/wget "http://54.226.64.162/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || sudo systemctl start nginx && echo "webserver started"
+curl -I "http://54.226.64.162/" 2>&1 >> results1.txt
+echo "Starting backup process: "
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+aws s3 cp results1.txt s3://nginx-web-server-bucket/
 #sleep 10
 #done
 
