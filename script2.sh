a409819c8972c888861d87d6d25c9133e6bf06b3
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-17 17:29:56 +0530

    adding script2 to check errors

--- script2.sh
+++ script2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-aws ssm send-command \
-	--document-name "AWS-RunShellScript" \
-	--targets '[{"Key":"InstanceIds","Values":["i-03377a80bb4152d26"]}]' \
-	--parameters '{"commands":["#!/bin/bash","sudo -i","cd /var/log/nginx","tar -czf logs_"`date +"%d-%m-%Y"`".tgz *.gz"]}'
-scp -i kplabs-terraform.pem ec2-user@ec2-54-209-117-2.compute-1.amazonaws.com:/var/log/nginx/logs_"`date +"%d-%m-%Y"`".tgz .
-aws s3 cp logs_"`date +"%d-%m-%Y"`".tgz *.gz s3://nginx-web-server-bucket/
+#Create one compressed files from the daily logs
+aws ssm send-command --document-name "AWS-RunShellScript" --targets '[{"Key":"InstanceIds","Values":["i-03377a80bb4152d26"]}]' --parameters '{"commands":["#!/bin/bash","sudo -i","cd /var/log/nginx","tar -czf logs.tgz *.gz"]}'
+#Move the compressed file to the same location as script is running
+scp -i kplabs-terraform.pem root@ec2-54-209-117-2.compute-1.amazonaws.com:/var/log/nginx/logs.tgz /mnt/c/Users/user/Desktop/Git/
+#Rename the compressed log file with timestamps
+mv logs.tgz logs_"`date +"%d-%m-%Y"`".tgz
+#Upload compressed log file to AWS S3
+aws s3 cp logs_"`date +"%d-%m-%Y"`".tgz s3://nginx-web-server-bucket/
+#Remove the log file
 rm -rf logs_"`date +"%d-%m-%Y"`".tgz
