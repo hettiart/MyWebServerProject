de1c7e26963428c61f47b23ae57c47dd17004171
root <root@LAPTOP-78IE47PR.localdomain>
2021-02-18 17:34:44 +0530

    add scripts to github

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 #ssh to the public server
 ssh -i "kplabs-terraform.pem" ec2-user@ec2-54-209-117-2.compute-1.amazonaws.com
 #check if the webserver is running & and serving the expected content if not start it
-/usr/bin/wget "http://54.209.117.2/" --timeout 30 -O - 2>/dev/null | grep "nginx" || sudo systemctl start nginx
+/usr/bin/wget "http://54.209.117.2/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified"  || sudo systemctl start nginx
 #Getting the results of the web to a file along with time stamp
 curl -I "http://54.209.117.2/" 2>&1 >> results1.txt
 #Backup the results to AWS S3
--- script2.sh
+++ script2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#Create one compressed files from the daily logs
+#Create one compressed file from the daily logs
 aws ssm send-command --document-name "AWS-RunShellScript" --targets '[{"Key":"InstanceIds","Values":["i-03377a80bb4152d26"]}]' --parameters '{"commands":["#!/bin/bash","sudo -i","cd /var/log/nginx","tar -czf logs.tgz *.gz"]}'
 #Move the compressed file to the same location as script is running
-scp -i kplabs-terraform.pem root@ec2-54-209-117-2.compute-1.amazonaws.com:/var/log/nginx/logs.tgz /mnt/c/Users/user/Desktop/Git/
+#scp -i kplabs-terraform.pem root@ec2-54-209-117-2.compute-1.amazonaws.com:/var/log/nginx/logs.tgz .
+#ssh -i kplabs-terraform.pem root@i-03377a80bb4152d26
+scp -i kplabs-terraform.pem root@i-03377a80bb4152d26:/var/log/nginx/logs.tgz .
 #Rename the compressed log file with timestamps
 mv logs.tgz logs_"`date +"%d-%m-%Y"`".tgz
 #Upload compressed log file to AWS S3
