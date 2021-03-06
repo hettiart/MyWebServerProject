53761cda808c0e08f83b8de7532d3c02f275bac0
root <root@LAPTOP-78IE47PR.localdomain>
2021-03-06 19:41:14 +0530

    dos2unix change in script2.sh

--- script2.sh
+++ script2.sh
@@ -5,5 +5,5 @@ aws ssm send-command --document-name "AWS-RunShellScript" --targets '[{"Key":"In
 scp -i kplabs-terraform.pem root@i-03377a80bb4152d26:/var/log/nginx/logs.tgz /mnt/c/Users/user/Desktop/Git/logs.tgz
 #Upload compressed log file to AWS S3
 aws s3 cp '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r' s3://nginx-web-server-bucket/logs.tgz
-#Remove the log file
+#Remove the log file after S3 copy
 rm '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r'
