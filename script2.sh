540041e29233347845957f7cd5f685f9c1bb4d57
root <root@LAPTOP-78IE47PR.localdomain>
2021-03-06 20:22:57 +0530

    checking errors

--- script2.sh
+++ script2.sh
@@ -4,6 +4,6 @@ aws ssm send-command --document-name "AWS-RunShellScript" --targets '[{"Key":"In
 #Move the compressed file to the same location as script is running
 scp -i kplabs-terraform.pem root@i-03377a80bb4152d26:/var/log/nginx/logs.tgz /mnt/c/Users/user/Desktop/Git/logs.tgz
 #Upload compressed log file to AWS S3
-aws s3 cp '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r' s3://nginx-web-server-bucket/logs.tgz
-#Remove the log file after S3 copy
-rm '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r'
+aws s3 cp /mnt/c/Users/user/Desktop/Git/'logs.tgz'$'\r' s3://nginx-web-server-bucket/logs.tgz
+#Remove the log file
+rm /mnt/c/Users/user/Desktop/Git/'logs.tgz'$'\r'
