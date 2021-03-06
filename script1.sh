7f29261988aa59272da24fe39069bdc0fc4f3fec
root <root@LAPTOP-78IE47PR.localdomain>
2021-03-06 19:09:15 +0530

    dos2unix change

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #ssh to the public server
-ssh -i /mnt/c/Users/user/Desktop/Git/kplabs-terraform.pem ec2-user@ec2-34-234-203-145.compute-1.amazonaws.com
+ssh -i /mnt/c/Users/user/Desktop/Git/kplabs-terraform.pem ec2-user@ec2-34-234-203-145.compute-1.amazonaws.com 'sleep 1 >/dev/null &'
 #check if the webserver is running & and serving the expected content if not start it
 /usr/bin/wget "http://34.234.203.145/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || aws ssm send-command --document-name "AWS-RunShellScript" --parameters 'commands=["sudo systemctl start nginx"]' --targets "Key=instanceids,Values=i-03377a80bb4152d26"
 #Getting the results of the web to a file along with time stamp
