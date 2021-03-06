7f286f6256eeb77c6c0ea5e38de2a89bf11cf0a7
root <root@LAPTOP-78IE47PR.localdomain>
2021-03-06 17:34:29 +0530

    Final changes on scripts

--- script1.sh
+++ script1.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #ssh to the public server
-#ssh -i "kplabs-terraform.pem" ec2-user@ec2-54-209-117-2.compute-1.amazonaws.com
-ssh -i "kplabs-terraform.pem" ec2-user@ec2-34-234-203-145.compute-1.amazonaws.com
+ssh -i /mnt/c/Users/user/Desktop/Git/kplabs-terraform.pem ec2-user@ec2-34-234-203-145.compute-1.amazonaws.com
 #check if the webserver is running & and serving the expected content if not start it
-#/usr/bin/wget "http://54.209.117.2/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified"  || sudo systemctl start nginx
-/usr/bin/wget "http://34.234.203.145/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified"  || sudo systemctl start nginx
+/usr/bin/wget "http://34.234.203.145/" --timeout 30 -O - 2>/dev/null | grep "nginx" && echo "webserver is alive and content verified" || aws ssm send-command --document-name "AWS-RunShellScript" --parameters 'commands=["sudo systemctl start nginx"]' --targets "Key=instanceids,Values=i-03377a80bb4152d26"
 #Getting the results of the web to a file along with time stamp
 curl -I "http://34.234.203.145/" 2>&1 >> results1.txt
 #Backup the results to AWS S3
 echo "Starting backup process: "
-aws s3 cp results1.txt s3://nginx-web-server-bucket/
+aws s3 cp  'results1.txt'$'\r' s3://nginx-web-server-bucket/results1.txt
--- script2.sh
+++ script2.sh
@@ -2,12 +2,8 @@
 #Create one compressed file from the daily logs
 aws ssm send-command --document-name "AWS-RunShellScript" --targets '[{"Key":"InstanceIds","Values":["i-03377a80bb4152d26"]}]' --parameters '{"commands":["#!/bin/bash","sudo -i","cd /var/log/nginx","tar -czf logs.tgz *.gz"]}'
 #Move the compressed file to the same location as script is running
-#scp -i kplabs-terraform.pem root@ec2-54-209-117-2.compute-1.amazonaws.com:/var/log/nginx/logs.tgz .
-#ssh -i kplabs-terraform.pem root@i-03377a80bb4152d26
-scp -i kplabs-terraform.pem root@i-03377a80bb4152d26:/var/log/nginx/logs.tgz .
-#Rename the compressed log file with timestamps
-mv logs.tgz logs_"`date +"%d-%m-%Y"`".tgz
+scp -i kplabs-terraform.pem root@i-03377a80bb4152d26:/var/log/nginx/logs.tgz /mnt/c/Users/user/Desktop/Git/logs.tgz
 #Upload compressed log file to AWS S3
-aws s3 cp logs_"`date +"%d-%m-%Y"`".tgz s3://nginx-web-server-bucket/
+aws s3 cp '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r' s3://nginx-web-server-bucket/logs.tgz
 #Remove the log file
-rm -rf logs_"`date +"%d-%m-%Y"`".tgz
+rm '/mnt/c/Users/user/Desktop/Git/logs.tgz'$'\r'
